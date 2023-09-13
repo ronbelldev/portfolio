30f6d060871ccfe30cbcd7656b066ca5708a781b
ronbelldev <ronbelldev@gmail.com>
2023-09-13 16:33:54 +0200

    deployment test

---
+++ src/components/PageHeader/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.scss'
+
+const PageHeader = ({}) =>
+  <div className='page-header'>
+  </div>
+
+PageHeader.propTypes = {}
+
+export default PageHeader
---
+++ src/components/PageHeader/stories.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageHeader from './'
+
+export default {
+    title: 'PageHeader',
+    parameters: {
+        layout: 'centered',
+    },
+}
+
+export const Default = () =>
+    <PageHeader />
+ Default.storyName = 'default'
